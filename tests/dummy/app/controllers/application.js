1b762653ba21b6080ac96f7b4be6ae406aa851bc
terrance <terrancebryant85@gmail.com>
2018-05-12 20:35:46 -0700

    travis setup

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,12 +1,10 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-  tester: [{ first: 'terrance', last: 'bryant'}, { first: 'joyce', last: 'bryant'}],
 
   actions: {
     click(event, args) {
-      console.log(event, args)
-      console.log('button has been clicked')
+      
     }
   }
 });
